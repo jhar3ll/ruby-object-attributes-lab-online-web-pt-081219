1cb4b59f6e9ff80e0523c28a6c47831089456dac
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-21 00:14:31 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def person 
+    @person = person
+  end
+  
+  def person=(person)
+    @person = person 
+  end 
+  
+  def job 
+    @job = job
+  end 
+  
+  def job=(job)
+    @job = job
+  end
+  
+end
